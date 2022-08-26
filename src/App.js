e0e1f90d8070361465d9b66f4a916b05e73c39c2
giorgi <giorgi.doreuli3721@gmail.com>
2022-08-27 02:03:57 +0400

    created routes for first page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import FirstPage from './Components/FirstPage';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+            <Route exact path='/' element={<FirstPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
