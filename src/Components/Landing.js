7e810c5429d950471967f0f0f388168aa08fcdd7
giorgi <giorgi.doreuli3721@gmail.com>
2022-08-28 00:37:31 +0400

    fixed wrong path

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -8,7 +8,7 @@ function Landing() {
         <h2 className='Landing-heading'>Redberry</h2>
         <img src='Landing-thumbnail.png' alt='Landing-thumbnail' className='Landing-thumbnail'/>
         <div className='Landing-buttons'>
-          <Link to='/add'>ჩანაწერის დამატება</Link>
+          <Link to='/personalInfo'>ჩანაწერის დამატება</Link>
           <Link to='/list'>ჩანაწერების სია</Link>
         </div>
     </div>
