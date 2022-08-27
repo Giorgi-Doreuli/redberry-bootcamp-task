f523fbf004c01eca55ff0e43e630dd8d38bb2ea3
giorgi <giorgi.doreuli3721@gmail.com>
2022-08-28 00:12:25 +0400

    created routes and styled buttons in landing page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Landing.css'
-
+import {Link} from 'react-router-dom'
+ 
 function Landing() {
   return (
     <div className='Landing'>
+        <h2 className='Landing-heading'>Redberry</h2>
         <img src='Landing-thumbnail.png' alt='Landing-thumbnail' className='Landing-thumbnail'/>
+        <div className='Landing-buttons'>
+          <Link to='/add'>ჩანაწერის დამატება</Link>
+          <Link to='/list'>ჩანაწერების სია</Link>
+        </div>
     </div>
   )
 }
