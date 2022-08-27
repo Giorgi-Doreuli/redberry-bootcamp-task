c34b8ad6acbdb8152ad91db97f6934976bae0fdc
giorgi <giorgi.doreuli3721@gmail.com>
2022-08-28 00:11:03 +0400

    imported personal and laptop info components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Landing from './Components/Landing';
+import PersonalInfo from './Components/PersonalInfo'
+import LaptopInfo from './Components/LaptopInfo';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <div className="App">
         <Routes>
             <Route exact path='/' element={<Landing />} />
+            <Route path='/personalInfo' element={<PersonalInfo />}/>
+            <Route path='/laptopInfo' element={<LaptopInfo />}/>
         </Routes>
       </div>
     </Router>
