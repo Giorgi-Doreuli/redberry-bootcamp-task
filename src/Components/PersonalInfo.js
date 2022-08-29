9301a85fe379ba52435f72d2ca3365f7a7e61e0e
giorgi <giorgi.doreuli3721@gmail.com>
2022-08-29 19:10:37 +0400

    added selection part of personalInfo page

--- src/Components/PersonalInfo.js
+++ src/Components/PersonalInfo.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './PersonalInfo.css'
 
 function PersonalInfo() {
+
+const [teams, setTeams] = useState([]);
+const [positions, setPositions] = useState([]);
+
+useEffect(() =>{
+    const getTeams = async () => {
+        const api_url = 'https://pcfy.redberryinternship.ge/api/teams';
+        const fetchTeams = await fetch(api_url);
+        const fetchedTeams = await fetchTeams.json();
+        setTeams(fetchedTeams.data);
+    }
+
+    const getPositions = async () => {
+        const api_url = 'https://pcfy.redberryinternship.ge/api/positions';
+        const fetchPositions = await fetch(api_url);
+        const fetchedPositions = await fetchPositions.json();
+        setPositions(fetchedPositions.data);
+    }
+
+
+    getTeams();
+    getPositions();
+}, [])
+
   return (
     <div className='personalInfo'>
         <div className='personalInfo-headers'>
@@ -21,6 +45,30 @@ function PersonalInfo() {
                         <input  type="text" placeholder='ბაგრატიონი' className='username-input'
                         required />
                     </div>
+                </div>
+                <div className='teams'>   
+                    <select defaultValue={'DEFAULT'} className='teams-select'>
+                            <option value="DEFAULT" disabled>თიმი</option>
+                            {teams.map(item => (
+                            <option
+                            value={item.name}
+                            >
+                            {item.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className='positions'>   
+                    <select defaultValue={'DEFAULT'} className='positions-select'>
+                            <option value="DEFAULT" disabled>პოზიცია</option>
+                            {positions.map(item => (
+                            <option
+                            value={item.name}
+                            >
+                            {item.name}
+                            </option>
+                        ))}
+                    </select>
                 </div>
             </div>
         </div>
