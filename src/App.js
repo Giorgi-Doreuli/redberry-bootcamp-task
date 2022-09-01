0133dc68cfa9c831f045135cbf420739c75006ab
giorgi <giorgi.doreuli3721@gmail.com>
2022-09-01 20:18:34 +0400

    created route for submit page and imported it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Landing from './Components/Landing';
 import PersonalInfo from './Components/PersonalInfo'
 import LaptopInfo from './Components/LaptopInfo';
+import SubmitPage from './Components/SubmitPage';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
             <Route exact path='/' element={<Landing />} />
             <Route path='/personalInfo' element={<PersonalInfo/>}/>
             <Route path='/laptopInfo' element={<LaptopInfo />}/>
+            <Route path='/submitPage' element={<SubmitPage />}/>
         </Routes>
       </div>
     </Router>
