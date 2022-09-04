55914611b5f827fd15150458dc3d6f9022b2e8f8
giorgi <giorgi.doreuli3721@gmail.com>
2022-09-04 20:30:58 +0400

    added info in card and after click on see more button, user will see more detailed info of laptop and owner

---
+++ src/Components/Card.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useNavigate} from 'react-router-dom'
+import './Card.css'
+
+function Card(props) {
+
+    const navigate = useNavigate();
+    
+  return (
+    <div className="card">
+        <img src={`https://pcfy.redberryinternship.ge/${props.img}`} className="cardImg" alt={props.m}/>
+        <div className="cardWrapper">
+            <div className="cardInfo">
+                <div className="cardInfoNameSurname">
+                    <p className="cardInfoName">{props.userName}</p>
+                    <p className="cardInfoSurName">{props.userSurName}</p>
+                </div>
+                <p className="cardInfoLaptopName">{props.laptopName}</p>
+            </div>
+            <div className="seeMoreInfo" onClick={() => navigate(`/laptopList/${props.id}`)}>
+                <p className="seeMoreInfoNameP">მეტის ნახვა</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
